5b8d785e3be3a1d6b186d185e4ecb26113ee2266
Slava Pogosov <pogoslav1@gmail.com>
2023-11-22 22:16:50 +0300

    1.0.1 [return element]

---
+++ vktools/template.py
@@ -0,0 +1,46 @@
+import json
+from typing_extensions import List, Dict, Any, Union, Optional
+from .keyboard import Text, OpenLink, Location, VkApps, VkPay
+
+class Element:
+  def __new__(
+    cls,
+    title: Optional[str] | None=None,
+    description: Optional[str] | None=None,
+    photo_id: Optional[str] | None=None,
+    link: Optional[str] | None=None,
+    buttons: Optional[List[Union[Text,OpenLink,Location,VkPay,VkApps]]] | None=None, 
+    template_type: str="open_link"  
+  ) -> Union[Optional[Dict[str, Any]], None]:
+    element: Dict[str, Any] = {
+      "title": title,
+      "description": description,
+      "photo_id": photo_id,
+      "buttons": buttons
+    }
+    if template_type == "open_link":
+      element["action"] = {
+        "type": "open_link",
+        "link": link
+      }
+    elif template_type == "open_photo":
+      element["action"] = {
+        "type": "open_photo"
+      }
+    else:
+      raise ValueError("Parameter template_type have: open_link or open_photo")
+    return element
+
+class Carousel:
+
+  def __init__(self, elements: List[Element]) -> None:
+    self.elements = elements
+  def add_carousel(self) -> str:
+    obj = json.dumps(
+      {
+        "type": "carousel",
+        "elements": self.elements
+      },
+      ensure_ascii=False
+    ).encode("utf-8")
+    return obj.decode("utf-8")
